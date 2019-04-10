b2c412f3662dcadad28338bbbfe7f1ddb8d67782
Nicholas Solem <nicholas.j.solem@ndsu.edu>
2019-04-10 13:18:28 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule
